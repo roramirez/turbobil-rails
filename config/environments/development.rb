cea934755fae53c88088f1a25683cc62e2490993
Rodrigo Ramírez Norambuena <a@rodrigoramirez.com>
2016-02-23 14:59:59 -0500

    change cofig of development enviroment to load fast

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,7 +25,7 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true
+  config.assets.debug = false
 
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
@@ -36,5 +36,5 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   #http://stackoverflow.com/a/18463187
-  config.reload_classes_only_on_change = false
+  config.reload_classes_only_on_change = true
 end
